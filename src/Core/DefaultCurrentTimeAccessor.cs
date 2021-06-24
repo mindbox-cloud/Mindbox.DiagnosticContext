c24e9fd0ace00d4c5a468180d8a83d54e9b4d217
Шоборов Роман <shoborov@mindbox.ru>
2021-06-24 17:07:06 +0300

    Default date-time accessor (#85)

---
+++ src/Core/DefaultCurrentTimeAccessor.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Mindbox.DiagnosticContext
+{
+    public class DefaultCurrentTimeAccessor : ICurrentTimeAccessor
+    {
+        public DateTime CurrentDateTimeUtc => DateTime.UtcNow;
+    }
+}
