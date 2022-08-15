e4c2a2c5fbb3223e37aa1632b6f6c78ef5943d13
TimurMannapov <mannapov@mindbox.ru>
2022-08-15 23:36:39 +0300

    Editorconfig

--- src/Core/CpuTimeByFeatureMetrics/IProcessTotalProcessorTimeProvider.cs
+++ src/Core/CpuTimeByFeatureMetrics/IProcessTotalProcessorTimeProvider.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#nullable disable
-
 using System;
 
 namespace Mindbox.DiagnosticContext.CpuTimeByFeatureMetrics;
