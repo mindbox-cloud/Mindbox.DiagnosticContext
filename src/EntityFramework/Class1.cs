2853311fd167a2ac4e8ad0ed456186bf475b27bb
DmitriiSkvortsov <skvortsov@mindbox.cloud>
2023-10-25 11:56:10 +0300

    цшз

--- src/EntityFramework/Class1.cs
+++ src/EntityFramework/Class1.cs
@@ -1,36 +1,49 @@
-﻿namespace Mindbox.DiagnosticContext.EntityFramework;
+﻿using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
-public interface IEfExecutedCommandsMetricProvider
+namespace Mindbox.DiagnosticContext.EntityFramework;
+
+public class EfExecutedCommandsMetrics
 {
-	long NumOfExecutedCommands { get; }
+	private static readonly AsyncLocal<EfExecutedCommandsMetrics> _instance = new();
+
+	public static EfExecutedCommandsMetrics Instance
+	{
+		get
+		{
+			return _instance.Value ??= new EfExecutedCommandsMetrics();
+		}
+	}
+
+	public long NumOfExecutedCommands { get; private set; }
+
+	public void ReportCommandStarted()
+	{
+		NumOfExecutedCommands++;
+	}
 }
 
 public sealed class EfExecutedCommandsMetricsType : MetricsType<EfExecutedCommandsMeasurer>
 {
-	private readonly IEfExecutedCommandsMetricProvider _executedCommandsMetricProvider;
-
-	public EfExecutedCommandsMetricsType(IEfExecutedCommandsMetricProvider executedCommandsMetricProvider)
-		: base(new DefaultCurrentTimeAccessor(), "SqlCommandsExecuted")
+	public EfExecutedCommandsMetricsType() : base(new DefaultCurrentTimeAccessor(), "SqlCommandsExecuted")
 	{
-		_executedCommandsMetricProvider = executedCommandsMetricProvider;
 	}
 
 	public override string Units => "[commands]";
 
-	protected override EfExecutedCommandsMeasurer CreateMeasurerCore() => new(CurrentTimeAccessor, _executedCommandsMetricProvider);
+	protected override EfExecutedCommandsMeasurer CreateMeasurerCore() => new(CurrentTimeAccessor);
 }
 
 public sealed class EfExecutedCommandsMeasurer : MetricsMeasurer
 {
-	private readonly IEfExecutedCommandsMetricProvider _executedCommandsMetricProvider;
-
 	private long _commandsExecutedOnStop;
 	private long _commandsExecutedOnStart;
 
-	public EfExecutedCommandsMeasurer(ICurrentTimeAccessor currentTimeAccessor, IEfExecutedCommandsMetricProvider executedCommandsMetricProvider)
+	public EfExecutedCommandsMeasurer(ICurrentTimeAccessor currentTimeAccessor)
 		: base(currentTimeAccessor, "EfExecutedCommands")
 	{
-		_executedCommandsMetricProvider = executedCommandsMetricProvider;
 	}
 
 	protected override long? GetValueCore()
@@ -40,11 +53,92 @@ public sealed class EfExecutedCommandsMeasurer : MetricsMeasurer
 
 	protected override void StartCore()
 	{
-		_commandsExecutedOnStart = _executedCommandsMetricProvider.NumOfExecutedCommands;
+		_commandsExecutedOnStart = EfExecutedCommandsMetrics.Instance.NumOfExecutedCommands;
 	}
 
 	protected override void StopCore()
 	{
-		_commandsExecutedOnStop = _executedCommandsMetricProvider.NumOfExecutedCommands;
+		_commandsExecutedOnStop = EfExecutedCommandsMetrics.Instance.NumOfExecutedCommands;
+	}
+}
+
+public class EfCommandsScorerInterceptor : DbCommandInterceptor
+{
+	public override InterceptionResult<DbDataReader> ReaderExecuting(
+		DbCommand command,
+		CommandEventData eventData,
+		InterceptionResult<DbDataReader> result) => ReportCommandStarted(result);
+
+	public override InterceptionResult<object> ScalarExecuting(
+		DbCommand command,
+		CommandEventData eventData,
+		InterceptionResult<object> result) => ReportCommandStarted(result);
+
+	public override InterceptionResult<int> NonQueryExecuting(
+		DbCommand command,
+		CommandEventData eventData,
+		InterceptionResult<int> result) => ReportCommandStarted(result);
+
+	public override async ValueTask<InterceptionResult<DbDataReader>> ReaderExecutingAsync(
+		DbCommand command,
+		CommandEventData eventData,
+		InterceptionResult<DbDataReader> result,
+		CancellationToken cancellationToken = default) => ReportCommandStarted(result);
+
+	public override async ValueTask<InterceptionResult<object>> ScalarExecutingAsync(
+		DbCommand command,
+		CommandEventData eventData,
+		InterceptionResult<object> result,
+		CancellationToken cancellationToken = default) => ReportCommandStarted(result);
+
+	public override async ValueTask<InterceptionResult<int>> NonQueryExecutingAsync(
+		DbCommand command,
+		CommandEventData eventData,
+		InterceptionResult<int> result,
+		CancellationToken cancellationToken = default) => ReportCommandStarted(result);
+
+	public override DbDataReader ReaderExecuted(
+		DbCommand command,
+		CommandExecutedEventData eventData,
+		DbDataReader result) => ReportCommandFinished(result);
+
+	public override object? ScalarExecuted(
+		DbCommand command,
+		CommandExecutedEventData eventData,
+		object? result) => ReportCommandFinished(result);
+
+	public override int NonQueryExecuted(
+		DbCommand command,
+		CommandExecutedEventData eventData,
+		int result) => ReportCommandFinished(result);
+
+	public override async ValueTask<DbDataReader> ReaderExecutedAsync(
+		DbCommand command,
+		CommandExecutedEventData eventData,
+		DbDataReader result,
+		CancellationToken cancellationToken = default) => ReportCommandFinished(result);
+
+	public override async ValueTask<object?> ScalarExecutedAsync(
+		DbCommand command,
+		CommandExecutedEventData eventData,
+		object? result,
+		CancellationToken cancellationToken = default) => ReportCommandFinished(result);
+
+	public override async ValueTask<int> NonQueryExecutedAsync(
+		DbCommand command,
+		CommandExecutedEventData eventData,
+		int result,
+		CancellationToken cancellationToken = default) => ReportCommandFinished(result);
+
+	private static T ReportCommandStarted<T>(T result)
+	{
+		EfExecutedCommandsMetrics.Instance.ReportCommandStarted();
+		return result;
+	}
+
+	private static T ReportCommandFinished<T>(T result)
+	{
+		// NOTE: for future purpose (example: execution time)
+		return result;
 	}
 }
