c49ee1dcc2379f02cce85fdd46f73bfa15e03753
ilyaaxenov <aksenov.i@mindbox.cloud>
2026-06-10 12:13:37 +0300

    *

--- src/AspNetCoreTestProject/Controllers/TestController.cs
+++ src/AspNetCoreTestProject/Controllers/TestController.cs
@@ -1,11 +1,11 @@
 // Copyright 2021 Mindbox Ltd
-//
+// 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 using System;
-using System.Threading;
 using Microsoft.AspNetCore.Mvc;
 using Mindbox.DiagnosticContext.AspNetCore;
 
@@ -29,13 +28,9 @@ public class TestController : Controller
 
 		using (diagnosticContext.Measure("outer"))
 		{
-			Thread.Sleep(TimeSpan.FromMilliseconds(new Random().Next(1, 100)));
 			diagnosticContext.ReportValue("reported", DateTime.Now.Minute);
 			using (diagnosticContext.Measure("inner"))
-			{
-				Thread.Sleep(TimeSpan.FromMilliseconds(new Random().Next(1, 100)));
 				return Ok();
-			}
 		}
 	}
 }
